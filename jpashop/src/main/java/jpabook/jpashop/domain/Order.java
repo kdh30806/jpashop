0c7cb381cdf4360abbe5b287b82f594f0522c9ff
kdh30806 <kdh30806@naver.com>
2022-03-27 22:38:42 +0900

    주문, 주문상품 도메인 비즈니스 로직 구현

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -46,7 +46,7 @@ public class Order {
 	private LocalDateTime orderDate; // 주문시간
 	
 	@Enumerated(EnumType.STRING)
-	private OrderStatus orderStatus;
+	private OrderStatus status;
 	
 	// 연관관계 메서드
 	public void setMember(Member member) {
@@ -62,5 +62,45 @@ public class Order {
 	public void setDelivery(Delivery delivery) {
 		this.delivery = delivery;
 		delivery.setOrder(this);
+	}
+	
+	public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+		Order order = new Order();
+		order.setMember(member);
+		order.setDelivery(delivery);
+		for(OrderItem orderItem : orderItems) {
+			order.addOrderItem(orderItem);
+		}
+		order.setStatus(OrderStatus.ORDER);
+		order.setOrderDate(LocalDateTime.now());
+		return order;
+	}
+	
+	/**
+	 * 주문 취소
+	 */
+	public void cancel() {
+		if(delivery.getStatus() == DeliveryStatus.COMP) { // 배송완료일때
+			throw new IllegalStateException("이미 배송완료된 상품은 최소가 불가능합니다.");			
+		}
+		
+		this.setStatus(OrderStatus.CANCEL);
+		for(OrderItem orderItem : orderItems) {
+			orderItem.cancel();
+		}
+	}
+	
+
+	/**
+	 * 전체 주문 가격 조회
+	 * @return totalPrice
+	 */
+	public int getTotalPrice() {
+		int totalPrice = 0;
+		for(OrderItem orderItem : orderItems) {
+			totalPrice += orderItem.getTotalPrice();
+		}
+		
+		return totalPrice;
 	}
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -31,6 +31,29 @@ public class OrderItem {
 	
 	private int count;
 	
+	// ** 생성 메서드 ** //
+	public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+		OrderItem orderItem = new OrderItem();
+		orderItem.setItem(item);
+		orderItem.setOrderPrice(orderPrice);
+		orderItem.setCount(count);
+		
+		item.removeStock(count); // 재고를 줄여줌
+		return orderItem;
+	}
+
+	// ** 비즈니스 로직 ** //
+	public void cancel() {
+		getItem().addStock(count); // 재고수량을 다시 늘려줌
+	}
+
 	
+	// ** 조회 로직 ** //
+	/**
+	 * 주문상품 전체 가격 조회
+	 */
+	public int getTotalPrice() {
+		return getOrderPrice() * getCount();
+	}
 	
 }
