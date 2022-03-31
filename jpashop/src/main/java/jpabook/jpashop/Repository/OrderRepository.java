e169f73cbc61758ffd942e9becf719e58852136d
kdh30806 <kdh30806@naver.com>
2022-04-01 00:06:46 +0900

    홈화면, 멤버 등록화면 구현 완료

--- jpashop/src/main/java/jpabook/jpashop/Repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/Repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.Repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.stereotype.Repository;
@@ -20,4 +22,13 @@ public class OrderRepository {
 	public Order findOne(Long id) {
 		return em.find(Order.class, id);
 	}
+	
+//	public List<Order> findAll(OrderSearch orderSearch){
+//		return em.createQuery("select o from Order o join o.member m" +
+//								" where o.status = :status" +
+//								" and m.name like :name", Order.class)
+//				.setParameter("status", orderSearch.getOrderStatus())
+//				.setParameter("name", orderSearch.getMemberName())
+//				.getResultList();
+//	}
 }
