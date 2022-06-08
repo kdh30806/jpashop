b484448258433db399e1df5b3117127e2f6b2abb
kdh30806 <kdh30806@naver.com>
2022-06-08 20:00:22 +0900

    2022.06.08 - 컬렉션 조회 패치 조인 페이징 최적화

--- jpashop/src/main/java/jpabook/jpashop/Repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/Repository/OrderRepository.java
@@ -14,63 +14,78 @@ import lombok.RequiredArgsConstructor;
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
-	
+
 	private final EntityManager em;
-	
+
 	public void save(Order order) {
 		em.persist(order);
 	}
-	
+
 	public Order findOne(Long id) {
 		return em.find(Order.class, id);
 	}
-	
+
 	public List<Order> findAllByString(OrderSearch orderSearch) {
 
-        String jpql = "select o from Order o join o.member m";
-        boolean isFirstCondition = true;
+		String jpql = "select o from Order o join o.member m";
+		boolean isFirstCondition = true;
 
-    //주문 상태 검색
-    if (orderSearch.getOrderStatus() != null) {
-        if (isFirstCondition) {
-            jpql += " where";
-            isFirstCondition = false;
-        } else {
-            jpql += " and";
-        }
-        jpql += " o.status = :status";
-    }
+		// 주문 상태 검색
+		if (orderSearch.getOrderStatus() != null) {
+			if (isFirstCondition) {
+				jpql += " where";
+				isFirstCondition = false;
+			} else {
+				jpql += " and";
+			}
+			jpql += " o.status = :status";
+		}
 
-    //회원 이름 검색
-    if (StringUtils.hasText(orderSearch.getMemberName())) {
-        if (isFirstCondition) {
-            jpql += " where";
-            isFirstCondition = false;
-        } else {
-            jpql += " and";
-        }
-        jpql += " m.name like :name";
-    }
+		// 회원 이름 검색
+		if (StringUtils.hasText(orderSearch.getMemberName())) {
+			if (isFirstCondition) {
+				jpql += " where";
+				isFirstCondition = false;
+			} else {
+				jpql += " and";
+			}
+			jpql += " m.name like :name";
+		}
 
-    TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-            .setMaxResults(1000);
+		TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
 
-    if (orderSearch.getOrderStatus() != null) {
-        query = query.setParameter("status", orderSearch.getOrderStatus());
-    }
-    if (StringUtils.hasText(orderSearch.getMemberName())) {
-        query = query.setParameter("name", orderSearch.getMemberName());
-    }
+		if (orderSearch.getOrderStatus() != null) {
+			query = query.setParameter("status", orderSearch.getOrderStatus());
+		}
+		if (StringUtils.hasText(orderSearch.getMemberName())) {
+			query = query.setParameter("name", orderSearch.getMemberName());
+		}
 
-    return query.getResultList();
-}
+		return query.getResultList();
+	}
 
 	public List<Order> findAllWithMemberDelivery() {
-		 return em.createQuery(
+		return em.createQuery("select o from Order o" + " join fetch o.member m" + " joing fetch o.delivery d",
+				Order.class).getResultList();
+	}
+	
+	public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+		return em.createQuery(
 				"select o from Order o" +
 						" join fetch o.member m" +
-						" joing fetch o.delivery d", Order.class				
-		).getResultList();
+						" join fetch o.delivery d", Order.class)
+				.setFirstResult(offset)
+				.setMaxResults(limit)
+				.getResultList();
+	}
+
+	public List<Order> findAllWithItem() {
+		return em.createQuery(
+				"select distinct  o from Order o" + 
+						" join fetch o.member m" + 
+						" join fetch o.delivery d" + 
+						" join fetch o.orderItems oi" + 
+						" join fetch oi.item i", Order.class).getResultList();
 	}
 
 }
--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.Repository.OrderRepository;
@@ -44,6 +45,27 @@ public class OrderApiController {
 			.map(o -> new OrderDto(o))
 			.collect(Collectors.toList());
 		return collect;
+	}
+	
+	// 페이징이 불가능하다.
+	// 패치 조인은 하나만 써야한다. = 1 X N X M 불가능하다.
+	@GetMapping("/api/v3/orders")
+	public List<OrderDto> ordersV3(){
+		List<Order> orders = orderRepository.findAllWithItem();
+		List<OrderDto> collect = orders.stream()
+				.map(o -> new OrderDto(o))
+				.collect(Collectors.toList());
+			return collect;
+	}
+	
+	@GetMapping("/api/v3.1/orders")
+	public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset, @RequestParam(value = "limit", defaultValue = "100") int limit){
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+		
+		List<OrderDto> collect = orders.stream()
+				.map(o -> new OrderDto(o))
+				.collect(Collectors.toList());
+			return collect;
 	}
 	
 	@Getter
